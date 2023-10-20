42447c2ec7a7d37e6bc0a4805489db33b7ec9536
AndreaLlerenaZ2003 <andrea.llerenaz2003@gmail.com>
2023-10-20 09:54:14 -0500

    se agrega routers a index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const v1UsuarioRouters = require("./v1/routers/UsuarioRouters");
-const v1DocumentoRouters = require("./v1/routers/DocumentoRouters");
-const v1CategoriaRouters = require("./v1/routers/CategoriaRouters");
-const v1CategoriaLugarRouters = require("./v1/routers/CategoriaLugarRouter")
-const {executeSqlQuery, executeSqlQueryGet } = require('./database/database'); 
+const v1UsuarioRouter = require("./v1/routers/UsuarioRouters");
+const v1DocumentoRouter = require("./v1/routers/DocumentoRouters");
+const v1CategoriaRouter = require("./v1/routers/CategoriaRouters");
+const v1CategoriaLugarRouter = require("./v1/routers/CategoriaLugarRouter");
+const v1FavoritoRouter = require("./v1/routers/FavoritoRouter");
+const v1LugarRecomendadoRouter = require("./v1/routers/LugarRecomendadoRouter");
+const v1ViajeRouter = require("./v1/routers/ViajeRouter");
+const v1ViajeLugarRouter = require("./v1/routers/ViajeLugarRouter");
+const v1LugarRouter = require("./v1/routers/LugarRouter");
+const v1ReseñaRouter = require("./v1/routers/ReseñaRouter");
+
+
+
+const {executeSqlQuery, executeSqlQueryGet,executeSqlQueryWithGet } = require('./database/database'); 
 const cors = require("cors");
 const bodyParser = require('body-parser');
 
@@ -12,9 +21,17 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/api/v1/UsuarioRouters", v1UsuarioRouters);
-app.use("/api/v1/DocumentoRouters", v1DocumentoRouters);
-app.use("/api/v1/CategoriaRouters", v1CategoriaRouters);
+app.use("/api/v1/UsuarioRouters", v1UsuarioRouter);
+app.use("/api/v1/DocumentoRouters", v1DocumentoRouter);
+app.use("/api/v1/CategoriaRouters", v1CategoriaRouter);
+app.use("/api/v1/CategoriaLugarRouters",v1CategoriaLugarRouter);
+app.use("/api/v1/FavoritoRouter",v1FavoritoRouter);
+app.use("/api/v1/LugarRecomendadoRouter",v1LugarRecomendadoRouter);
+app.use("/api/v1/ViajeRouter",v1ViajeRouter);
+app.use("/api/v1/ViajeLugarRouter",v1ViajeLugarRouter);
+app.use("api/v1/LugarRouter",v1LugarRouter);
+app.use("api/v1/ReseñaRouter",v1ReseñaRouter);
+
 
 
 app.listen(PORT, () => {
