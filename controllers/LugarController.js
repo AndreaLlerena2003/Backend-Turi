7d2d1f648465236182954d3a86c526ccc8179912
AndreaLlerenaZ2003 <andrea.llerenaz2003@gmail.com>
2023-11-14 13:59:53 -0500

    agregandofuncion banner

--- controllers/LugarController.js
+++ controllers/LugarController.js
@@ -2,6 +2,27 @@ const { executeSqlQuery, executeSqlQueryGet } = require('../database/database');
 
 class LugarController {
 //metodo para traer lugar recomendado segun id
+  static getBanner(){
+    const sqlQuery = `SELECT * FROM Lugar`;
+    executeSqlQueryGet(sqlQuery,(err,resultados)=> {
+      if(err){
+        callback(err);
+      }
+      else{
+        if(resultados.length == 0){
+          callback(null,[]);
+        }else{
+          const lugarC = [];
+          for(let i = 0;i<resultados.length;i++){
+            const l = resultados[i];
+            lugarC.push(l);
+
+          }
+          callback(null, lugarC);
+        }
+      }
+    })
+  }
   static traerLugarSegunLugarRecomendadoId(id, callback) {
     const sqlQuery = `SELECT * FROM Lugar WHERE Lugar.idTipoLugar = '${id}'`;
 
